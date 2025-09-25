66016846cf71661fd619154618346af29d184804
Dastan Hairushev <hairushevdastan@gmail.com>
2025-09-25 21:06:55 +0500

    feat: add faq section

--- portfolio/js/slider.js
+++
@@ -1,4 +0,0 @@
-const slider = document.querySelector('.portfolio__slider');
-const container = document.querySelector('.portfolio__slider-container');
-
-container.scrollLeft = (slider.scrollWidth - container.clientWidth) / 2;
