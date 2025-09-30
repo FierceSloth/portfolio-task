7a66bb160162c454b3d5647a022e883279b6cdb2
Dastan Hairushev <hairushevdastan@gmail.com>
2025-09-30 18:11:18 +0500

    feat: improved accordion logic

---
+++ portfolio/js/accordion.js
@@ -0,0 +1,17 @@
+const currentAccordId = localStorage.getItem("currentAccorId") || document.querySelector('details').id;
+
+document.querySelectorAll("details").forEach((other) => {
+  other.removeAttribute("open");
+});
+document.getElementById(currentAccordId).open = true;
+
+document.querySelectorAll("details").forEach((el) => {
+  el.addEventListener("toggle", () => {
+    if (el.open) {
+      document.querySelectorAll("details").forEach((other) => {
+        if (other !== el) other.removeAttribute("open");
+      });
+      localStorage.setItem("currentAccorId", el.id);
+    }
+  });
+});
--- portfolio/js/index.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll("details").forEach((el) => {
-  el.addEventListener("toggle", () => {
-    if (el.open) {
-      document.querySelectorAll("details").forEach((other) => {
-        if (other !== el) other.removeAttribute("open");
-      });
-    }
-  });
-});
